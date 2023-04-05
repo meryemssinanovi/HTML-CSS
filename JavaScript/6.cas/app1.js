26abfefab4b8faf0daca60952b3f092bd6c88ae7
meremssinanovi <merjemsinanovic867@gmail.com>
2023-04-05 23:01:47 +0200

    JS

--- JavaScript/6.cas/app1.js
+++ JavaScript/6.cas/app1.js
@@ -84,3 +84,31 @@ for (let i = 2; i < 21; i += 2) {
 for (let i = 6; i < 15; i++) {
   console.log(2 * i);
 }
+
+// 1. Ispisati neparne bojeve od 1 do 20 na dva nacina
+
+for (let i = 1; i < 21; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i < 21; i += 2) {
+  console.log(i);
+}
+
+// 2.Ispisati sve brojeve od 50 do 100 koji su deljivi sa 5
+
+for (let i = 50; i < 101; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+
+// 4. Suma neparnih prirodnih brojeva od 10 do 20.
+
+let zbir = 0;
+for (let i = 11; i < 20; i += 2) {
+  zbir += i;
+}
+console.log(zbir);
