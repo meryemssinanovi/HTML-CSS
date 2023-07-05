181bb38a47e0d9f848849558e55d7a09997391e7
meremssinanovi <merjemsinanovic867@gmail.com>
2023-07-05 20:54:31 +0200

    JS

---
+++ JavaScript/32.cas/app2.js
@@ -0,0 +1,17 @@
+// 4.
+
+const brojevi5 = [-5, 4, 3, 10, 14, -5];
+const naredni_niz = brojevi5.map((element) =>
+  element < 0 ? element * -1 : element ** 2
+);
+console.log(naredni_niz);
+
+// 1.
+const prvi_niz = ["krofna", "kifla", "knjiga", "posao"];
+const novi_niz = prvi_niz.map((element) => e.toUpperCase());
+console.log(novi_niz);
+
+// 2.
+
+const br3 = brojevi.map((element) => element ** 3);
+console.log(br3);
---
+++ JavaScript/33.cas/app1.js
@@ -0,0 +1,60 @@
+const godine = [17, 17, 21, 27, 19];
+// 1. Napraviti novi niz,koji ce sadrzati samo godine vece od 17.
+
+const godineVeceOd17 = [];
+godine.forEach((godina) => {
+  //   if (godina > 17) {
+  //     godineVeceOd17.push(godina);
+  //   }
+  godina > 17 && godineVeceOd17.push(godina);
+});
+console.log(godineVeceOd17);
+
+// filter() metoda pravi novi niz sa onli elementima koji su prosli test.
+// Novi niz moze biti prazan,iste duzine kao originalni ili imati manje elemenata od originalnog.
+// Zavisi od toga koliko je elemenata proslo test.
+// callback funkcija vraca uslov koji neki element treba da zadovoljava kako bi prosao test(kako bi bio dodat u novom nizu).
+
+const godineVeceOd16 = godine.filter((godina) => godina > 17);
+console.log(godineVeceOd16);
+
+// 1.
+// prvi deo. napraviti pomocni niz koji ce sadrzati elemente od oba niza.
+// drugi deo. napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve.
+// treci deo. u drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20
+// funkcija na kraju treba da vrati dvav napravljena pomocna niza
+
+function helpers(arr1, arr2) {
+  const helper1 = arr1.concat(arr2);
+  const helper2 = helper1.filter((broj) => broj % 2 === 0);
+  helper2.unshift(10, 20);
+  return `first helper: ${helper1}\n
+  second helper: ${helper2}`;
+}
+console.log(helpers([1, 2, 3, 4, 5], [6, 7, 8, 9]));
+
+const student = [
+  "Anastasija",
+  "Meryem",
+  "Emir",
+  "Nedim",
+  "Selver",
+  "Ajla",
+  "Asija",
+];
+// Napraviti novi niz koji filtrira postojeci i vraca novi sa onim elementima starog niza,gde je drugo slovo "e".
+
+const student2 = student.filter((student) => student[1] === "e");
+console.log(student2);
+
+// reduce() metoda izvrsava callback funkciju za svaki element jednom i vraca nam jednu vrednost.
+// reduce() metoda se izvrsava sa leve na desnu stranu
+// callback funkcija reduce metode moze imati 4 argumenta:
+// (previousValue , currentValue , index , array)
+
+const suma = godine.reduce((prev, curr) => prev + curr);
+console.log(suma);
+
+// Mozemo dodati pocetnu vrednost, ona predstavlja drugi parametar callback funkcije.
+const suma2 = godine.reduce((prev, curr) => prev + curr, 100);
+console.log(suma);
